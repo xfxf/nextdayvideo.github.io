abe62f075fa52602cde9cbb39d82338a4bc0ab27
Patrick Quinn-Graham <make-contact@pftqg.com>
2020-08-29 16:23:28 +1000

    New monitor (#10)
    * New monitor
    
    * Delete room1.html
    
    * Delete room2.html
    
    * Delete room3.html
    
    * Delete room4.html
    
    * Styles & index

---
+++ tools/monitor/dynamic.js
@@ -0,0 +1,107 @@
+const fetchState = async (liveEventName) => {
+  try {
+    const before = Date.now();
+    const fetchResponse = await fetch(`https://pyconlinestreammonitor.azurewebsites.net/api/stream/${liveEventName}`);
+    const state = await fetchResponse.json();
+
+    const totalTime = Date.now() - before;
+
+    console.log(`[${liveEventName}] Got state in ${totalTime}ms`, state);
+
+    return state;
+  } catch(err) {
+    return { error: err.message }
+  }
+}
+
+const wait = (time) => new Promise(resolve => setTimeout(resolve, time));
+
+const createSetTitleLabel = (el, room) => ({ loading = false, live = false, error }) => {
+  let description = '';
+  if (loading) {
+    description = '...';
+  } else if (error) {
+    description = `: ${error}`;
+  } else if (live) {
+    description = ' (live)';
+  } else {
+    description = ' (offline)';
+  }
+  el.textContent = `${room}${description}`;
+}
+
+const run = async () => {
+  const player = amp("azuremediaplayer", {
+    nativeControlsForTouch: false,
+    controls: true,
+    autoplay: true,
+    width: "100%",
+    height: "100%",
+    muted: true,
+    // logo: { enabled: false }
+  });
+
+  
+  const params = (new URL(location.href)).searchParams;
+
+  const liveEventName = params.get('event');
+  const displayName = params.get('name') || liveEventName;
+
+  if (!liveEventName) {
+    throw new Error('Event not set');
+  }
+
+  const offlineEl = document.getElementById('offline');
+  const roomNameEl = document.getElementById('room-name');
+
+  const setTitleLabel = createSetTitleLabel(roomNameEl, displayName);
+
+  let currentStreamURL;
+
+  do {
+    setTitleLabel({ loading: true });
+
+    const { error, streamURL, offlineReason } = await fetchState(liveEventName);
+
+    if (error) {
+      setTitleLabel({ error: error });
+      await wait(30000);
+      continue;
+    }
+
+    setTitleLabel({ live: !!streamURL });
+
+    if (streamURL) {
+      offlineEl.style.display = 'none';
+      if (streamURL !== currentStreamURL) {
+        player.src({
+          src: streamURL,
+          type: "application/vnd.ms-sstr+xml"
+        });
+      }
+    } else {
+      offlineEl.style.display = '';
+      offlineEl.querySelector('small').textContent = `${liveEventName}: ${offlineReason}`;
+
+      if (currentStreamURL) {
+        if (player.isFullscreen()) {
+          player.exitFullscreen();
+        }
+
+        player.pause();
+      }
+    }
+
+    currentStreamURL = streamURL;
+
+    const offset = 30000 + (Math.round(Math.random() * 10000) - 5000);
+
+
+    console.log(`[${liveEventName}] Next in ${offset}ms`);
+
+    await wait(offset);
+  } while(true);
+
+};
+
+run().catch(err => console.error('Failed somewhere', err));
